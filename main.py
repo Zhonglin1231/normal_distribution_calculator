511ba1aba29e80410f8f9a81f161738c5ed53111
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2023-05-21 14:50:14 +0800

    create interface, to be done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+import PySimpleGUI as sg
 
 while True:
     # initialize parameters
@@ -14,117 +15,177 @@ while True:
 
     # clean the screen
     os.system("cls")
+# -----------------------------------------------------------------
+#                   start interface
+# -----------------------------------------------------------------
+    # create layout_start
+    layout_start = [
+        [sg.Button("Start")],
+        [sg.Button("Exit")]
+    ]
 
-    # give command
-    command = input("Insert command (any key / exit)：")
-    if command == "exit":
+    # create window_start
+    window_start = sg.Window("Start", layout_start)
+
+    # read the window_start
+    event, values = window_start.read()
+    if event == "Start":
+        window_start.close()
+
+    elif event in ("Exit", None):
+        window_start.close()
         exit()
+# -----------------------------------------------------------------
+#                   choose file interface
+# -----------------------------------------------------------------
+    # create layout_choose_file
+    layout_file = [
+        [sg.InputText("File_Name", key="fileName")],
+        [sg.Button("Confirm")],
+        [sg.Button("Exit")]
+    ]
 
-    else:
-        file_name = input("Insert File name：")
-        print("File name is：", file_name)
+    window_file = sg.Window("Choose File", layout_file)
 
-        # check if the file exists
-        while True:
+    while True:
+        event_file, values_file = window_file.read()
+
+        if event_file == "Confirm":
+            # check if file exists
+            file_name = values_file["fileName"]
             check = os.path.isfile(file_name)
             if check == 0:
-                print("File not found")
-                file_name = input("Insert File name：")
+                sg.Popup("File not found")
             else:
-                print("File found")
+                sg.Popup(f"{file_name} found")
+                window_file.close()
                 break
 
-        # initialize data set
-        data_set = []
-        excel = xlrd.open_workbook(file_name)
-        sheet = excel.sheet_by_index(0)
+        elif event_file in ("Exit", None):
+            window_file.close()
+            exit()
 
-        # input to choose which specific range of data in excel to use
-        while True:
-            print("Insert range of row to use (1 ~", sheet.nrows, ")")
-            start_row = input("Start_row：")
-            end_row = input("End_row：")
-            print("Insert range of colon to use (1 ~", sheet.ncols, ")")
-            start_col = input("Start_col：")
-            end_col = input("End_col：")
+# -----------------------------------------------------------------
+#                   calculation interface
+# -----------------------------------------------------------------
+    # initialize data set
+    data_set = []
+    excel = xlrd.open_workbook(file_name)
+    sheet = excel.sheet_by_index(0)
 
+    # use interface to choose which specific range of data in excel to use
+    # create layout_choose_data
+    layout_data = [
+        [sg.Text("Row range (1 ~", sheet.nrows, ")")],
+        [sg.Text("Start_row："), sg.InputText(key="start_row")],
+        [sg.Text("End_row："), sg.InputText(key="end_row")],
+        [sg.Text("Colon range (1 ~", sheet.ncols, ")")],
+        [sg.Text("Start_col："), sg.InputText(key="start_col")],
+        [sg.Text("End_col："), sg.InputText(key="end_col")],
+        [sg.Button("Confirm")],
+        [sg.Button("Exit")]
+    ]
+
+    window_data = sg.Window("Choose Data", layout_data)
+
+    while True:
+        event_data, values_data = window_data.read()
+        if event_data == "Confirm":
             # check if the input is valid
-            while True:
-                try:
-                    start_row = int(start_row)
-                    end_row = int(end_row)
-                    start_col = int(start_col)
-                    end_col = int(end_col)
-                    break
-                except ValueError:
-                    print("Invalid input")
-                    print("Insert range of row to use (1 ~", sheet.nrows, ")")
-                    start_row = input("Start_row：")
-                    end_row = input("End_row：")
-                    print("Insert range of colon to use (1 ~", sheet.ncols, ")")
-                    start_col = input("Start_col：")
-                    end_col = input("End_col：")
+            try:
+                start_row = int(values_data["start_row"])
+                end_row = int(values_data["end_row"])
+                start_col = int(values_data["start_col"])
+                end_col = int(values_data["end_col"])
+            except ValueError:
+                sg.Popup("Invalid input")
+                continue
             if start_row > end_row or start_col > end_col or end_row > sheet.nrows or end_col > sheet.ncols:
-                print("Invalid input")
+                sg.Popup("Invalid input")
                 continue
 
             # append data to data set
             for i in range(int(start_row), int(end_row) + 1):
                 for j in range(int(start_col), int(end_col) + 1):
                     data_set.append(sheet.cell_value(i - 1, j - 1))
-            print("Data set is：", data_set)
+            sg.Popup("Data set is：", data_set)
+
             # ask if keep adding
-            command_add = input("Keep adding? (y / n)：")
-            if command_add == "n":
+            command_add = sg.PopupYesNo("Keep adding?")
+            if command_add == "No":
+                window_data.close()
                 break
 
-        # calculate mean and variance
-        mean = sum(data_set) / len(data_set)
-        variance = sum([(i - mean) ** 2 for i in data_set]) / len(data_set)
-        standard_deviation = np.sqrt(variance)
+            # exit the program
+            elif event_data in ("Exit", None):
+                window_data.close()
+                exit()
 
-        # print parameters for checking
-        print("Mean: ", mean)
-        print("Variance: ", variance)
-        print("Standard Deviation: ", standard_deviation)
+    # calculate mean and variance
+    mean = sum(data_set) / len(data_set)
+    variance = sum([(i - mean) ** 2 for i in data_set]) / len(data_set)
+    standard_deviation = np.sqrt(variance)
 
-        # draw the possibility curve
-        # why red line always very small!
-        x = np.linspace(mean - 3 * standard_deviation, mean + 3 * standard_deviation, 100)
-        y = stats.norm.pdf(x, mean, standard_deviation)
-        plt.plot(x, y, 'r--')
+    # whats the problem with my coming code?
+    # print the following command in the interface
+    layout_check = [
+        [sg.Text(f"Mean: {mean}")],
+        [sg.Text(f"Variance: {variance}")],
+        [sg.Text(f"Standard Deviation: {standard_deviation}")],
+        [sg.Button("Graph")],
+        [sg.Button("Calculate Possibility")],
+        [sg.Button("End")],
+        [sg.Button("Exit")]
+    ]
 
-        # give command for operation
-        while True:
-            command_in = input("Insert command (graph / cal_p / end)：")
-            # print the graph
-            if command_in == "graph":
-                plt.show()
+    window_check = sg.Window("Check", layout_check)
+    while True:
+        event_check, values_check = window_check.read()
+        if event_check == "Graph":
+            x = np.linspace(mean - 3 * standard_deviation, mean + 3 * standard_deviation, 100)
+            y = stats.norm.pdf(x, mean, standard_deviation)
+            plt.plot(x, y, 'r--')
 
-            elif command_in == "cal_p":
-                x1 = input("Insert lower bound：")
-                x2 = input("Insert higher bound：")
+        elif event_check == "Calculate Possibility":
+            # start a new interface and ask for x1 and x2
+            layout_possibility = [
+                [sg.Text("Insert lower bound："), sg.InputText(key="x1")],
+                [sg.Text("Insert higher bound："), sg.InputText(key="x2")],
+                [sg.Button("Confirm")],
+                [sg.Button("Exit")]
+            ]
 
-                # check if x1 and x2 are valid
-                while True:
+            window_possibility = sg.Window("Calculate Possibility", layout_possibility)
+
+            while True:
+                event_possibility, values_possibility = window_possibility.read()
+                if event_possibility == "Confirm":
+                    # check if x1 and x2 are valid
                     try:
-                        x1 = float(x1)
-                        x2 = float(x2)
-                        break
+                        x1 = float(values_possibility["x1"])
+                        x2 = float(values_possibility["x2"])
                     except ValueError:
-                        print("Invalid input")
-                        x1 = input("Insert lower bound：")
-                        x2 = input("Insert higher bound：")
+                        sg.Popup("Invalid input")
+                        continue
 
-                possibility = abs((stats.norm.cdf(x2, mean, standard_deviation) - stats.norm.cdf(x1, mean,
-                                                                                                 standard_deviation)
-                                   ) * 100)
+                    possibility = abs((stats.norm.cdf(x2, mean, standard_deviation) - stats.norm.cdf(x1, mean,
+                                                                                                     standard_deviation)
+                                       ) * 100)
 
-                # print the possibility with precision to 4 decimal places
-                print("Possibility: ", round(possibility, 2), "%")
+                    # print the possibility with precision to 4 decimal places
+                    sg.Popup("Possibility: ", round(possibility, 2), "%")
 
-            # start next analysis
-            elif command_in == "end":
-                # close the opened workbook
-                excel.release_resources()
-                break
+                elif event_check in ("Exit", None):
+                    window_check.close()
+                    break
+
+        elif event_check == "End":
+            excel.release_resources()
+            window_check.close()
+            break
+
+        elif event_check in ("Exit", None):
+            window_check.close()
+            exit()
+
+
