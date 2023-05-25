f2fff71c5107c9cd7457ea34c0a9708874ee0fe3
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2023-05-25 15:07:09 +0800

    scatter added

--- Chinese_version.py
+++ Chinese_version.py
@@ -26,54 +26,6 @@ plt.rcParams["axes.unicode_minus"] = False
 figure_num = [1]
 
 
-def choose_file_interface(cumulative_color):
-    # -----------------------------------------------------------------
-    #                   choose file interface
-    # -----------------------------------------------------------------
-    # create layout_choose_file
-    # choose file by clicking file
-    while True:
-        layout_file = [
-            [sg.FileBrowse(button_text="选择文件"), sg.In(key="文件名")],
-            [sg.Button("自定义")],
-            [sg.Button("快速分析")],
-            [sg.Button("退出")]
-        ]
-
-        window_file = sg.Window("选择文件", layout_file, size=(800, 500), location=(350, 200), element_justification='c')
-
-        while True:
-
-            event_file, values_file = window_file.read()
-
-            if event_file == "自定义":
-                # data if file exists
-                file_name = values_file["文件名"]
-                data = os.path.isfile(file_name)
-                if data == 0:
-                    sg.Popup("文件未找到")
-                else:
-                    window_file.close()
-                    calculation_interface(file_name, cumulative_color)
-
-            elif event_file == "快速分析":
-                # data if file exists
-                file_name = values_file["文件名"]
-                data = os.path.isfile(file_name)
-                if data == 0:
-                    sg.Popup("文件未找到")
-                else:
-                    window_file.close()
-                    rapid_calculation_interface(file_name, cumulative_color)
-
-            elif event_file in ("退出", None):
-                window_file.close()
-                sys.exit()
-
-            break
-            # calculate
-
-
 def calculation_interface(file_name, cumulative_color):
     # -----------------------------------------------------------------
     #                   calculation interface
@@ -82,19 +34,9 @@ def calculation_interface(file_name, cumulative_color):
         # initialize data set
         data_set = []
 
-        # read data from Excel
-        # 文件加载中
-        layout_loading = [
-            [sg.Text("文件加载中", font=("华文行楷", 20))],
-            [sg.Text("请稍等片刻", font=("华文行楷", 20))],
-        ]
+        # read file
 
-        window_loading = sg.Window("文件加载中", layout_loading, size=(800, 500), location=(350,200), element_justification='c')
-        window_loading.read(timeout=50)
-
-    # read file
-
-    # customize the settings
+        # customize the settings
         # pop up a window and ask for the value of header
         layout_header = [
             [sg.Text("请输入要定义的首行")],
@@ -102,7 +44,8 @@ def calculation_interface(file_name, cumulative_color):
             [sg.Button("确认")],
         ]
 
-        window_header = sg.Window("首行", layout_header, size=(800, 500), location=(350, 200), element_justification='c')
+        window_header = sg.Window("首行", layout_header, size=(800, 500), location=(350, 200),
+                                  element_justification='c')
         event_header, values_header = window_header.read()
         header = int(values_header["header"])
         window_header.close()
@@ -110,8 +53,6 @@ def calculation_interface(file_name, cumulative_color):
         csv = pd.read_csv(file_name, sep=',', header=header)
 
         nrows, ncols = csv.shape
-
-        window_loading.close()
 
         # use interface to choose which specific range of data in Excel to use
         # create layout_choose_data
@@ -127,7 +68,8 @@ def calculation_interface(file_name, cumulative_color):
             [sg.Button("退出")]
         ]
 
-        window_data = sg.Window("选择数据", layout_data, size=(800, 500), location=(350, 200), element_justification='c')
+        window_data = sg.Window("选择数据", layout_data, size=(800, 500), location=(350, 200),
+                                element_justification='c')
 
         while True:
             event_data, values_data = window_data.read()
@@ -152,8 +94,6 @@ def calculation_interface(file_name, cumulative_color):
                 if start_row > end_row or start_col > end_col or end_row > nrows or end_col > ncols:
                     sg.Popup("输入无效")
                     continue
-
-
 
                 # use csv.iat to append data in csv to data set
                 for i in range(int(start_row), int(end_row) + 1):
@@ -204,6 +144,7 @@ def rapid_calculation_interface(file_name, cumulative_color):
     # -----------------------------------------------------------------
     #                   rapid calculation interface
     # -----------------------------------------------------------------
+    global csv
     while True:
         # initialize data set
         data_set = []
@@ -214,13 +155,29 @@ def rapid_calculation_interface(file_name, cumulative_color):
         possibility = None
         col = 0
 
-        csv = pd.read_csv(file_name, sep=',', header=header)
-        nrows, ncols = csv.shape
+        nrows, ncols = 0, 0
+
+        # provide different colors
+        if cumulative_color % 6 == 1:
+            color = "r--"
+        elif cumulative_color % 6 == 2:
+            color = "b--"
+        elif cumulative_color % 6 == 3:
+            color = "g--"
+        elif cumulative_color % 6 == 4:
+            color = "y--"
+        else:
+            color = "c--"
 
         # use interface to choose which specific range of data in Excel to use
         # create layout_choose_data
         layout_data = [
-            [sg.Text(f"列范围 (1 ~ {ncols})"), sg.Text("列："), sg.InputText(key="列"), sg.Button("加载列")],
+            [sg.FileBrowse(button_text="新文件"), sg.In(key="新文件路径")],
+            [sg.Button("快速分析"), sg.Button("自定义")],
+            [sg.T("快速分析中...", key="快速分析中", visible=False),
+             sg.T("自定义中...", key="自定义中", visible=False)],
+            [sg.Text(f"列范围 (1 ~ {ncols})", key="列范围"), sg.Text("列："), sg.InputText(key="列"),
+             sg.Button("加载列")],
             # 告诉用户这是第几列数据
             [sg.Text(f"第{col}列数据为：", key="第几列")],
             [sg.Text(f"平均值: {round(mean, 4)}", key="平均值")],
@@ -230,15 +187,38 @@ def rapid_calculation_interface(file_name, cumulative_color):
              sg.InputText(key="x轴名称", size=(15, 1))],
             [sg.Button("概率计算"), sg.Text("最低值："), sg.InputText(key="x1", size=(8, 1)),
              sg.Text("最高值："), sg.In(key="x2", size=(8, 1)), sg.T("概率："), sg.T(f"{possibility}%", key="概率")],
-            [sg.Button("新文件")],
             [sg.Button("退出")]
         ]
 
-        window_data = sg.Window("选择数据", layout_data, size=(800, 500), location=(350, 200), element_justification='c')
+        window_data = sg.Window("选择数据", layout_data, size=(800, 500), location=(350, 200),
+                                element_justification='c')
 
         while True:
             while True:
                 event_data, values_data = window_data.read()
+                if event_data == "快速分析":
+                    file_name = values_data["新文件路径"]
+                    data = os.path.isfile(file_name)
+                    if data == 0:
+                        sg.Popup("文件未找到")
+                    else:
+                        csv = pd.read_csv(file_name, sep=',', header=header)
+                        nrows, ncols = csv.shape
+                        window_data["自定义中"].update(visible=False)
+                        window_data["快速分析中"].update("快速分析中...", visible=True)
+                        window_data["列范围"].update(f"列范围 (1 ~ {ncols})")
+
+                elif event_data == "自定义":
+                    # data if file exists
+                    file_name = values_data["新文件路径"]
+                    data = os.path.isfile(file_name)
+                    if data == 0:
+                        sg.Popup("文件未找到")
+                    else:
+                        calculation_interface(file_name, cumulative_color)
+                        window_data["自定义中"].update("自定义中...", visible=True)
+                        window_data["快速分析中"].update("快速分析中...", visible=False)
+
                 if event_data == "加载列":
                     # data if the input is valid
                     try:
@@ -268,6 +248,11 @@ def rapid_calculation_interface(file_name, cumulative_color):
 
                     print(data_set)
                     # calculate mean and variance
+                    if len(data_set) == 0:
+                        sg.Popup("请添加数据")
+                        # close the window and restart
+                        continue
+
                     mean = sum(data_set) / len(data_set)
                     # uses the technique of point estimate
                     variance = sum([(i - mean) ** 2 for i in data_set]) / len(data_set)
@@ -278,11 +263,6 @@ def rapid_calculation_interface(file_name, cumulative_color):
                     window_data["平均值"].update(f"平均值: {round(mean, 4)}")
                     window_data["平方差"].update(f"平方差: {round(variance, 4)}")
                     window_data["标准差"].update(f"标准差: {round(standard_deviation, 4)}")
-                    data_set = []
-
-                if event_data == "新文件":
-                    window_data.close()
-                    return
 
                 # sys.exit the program
                 elif event_data in ("退出", None):
@@ -292,16 +272,33 @@ def rapid_calculation_interface(file_name, cumulative_color):
                 # get the sum of the dataset, I don't want get nan
 
                 if event_data == "图像":
-                    x_label = values_data["x轴名称"]
-                    figure_num[0] += 1
-                    plt.figure(values_data["名称"])
-                    plt.title(values_data["名称"], size=20)
-                    plt.xlabel(x_label, size=15)
-                    plt.ylabel("概率密度", size=15)
-                    x = np.linspace(mean - 3 * standard_deviation, mean + 3 * standard_deviation, 100)
-                    y = stats.norm.pdf(x, mean, standard_deviation)
+                    # set the size of the graph
+                    plt.figure(figsize=(12, 5))
 
-                    # display the value of the mean, variance and standard deviation on the graph on appropriate position
+                    # set the title of the graph
+                    plt.figure(values_data["名称"])
+
+                    # first graph
+                    plt.subplot(1, 2, 1)
+
+
+                    # set values of x and y
+                    x1 = np.linspace(1, len(data_set), len(data_set))
+                    y1 = data_set
+                    # draw the line
+                    plt.scatter(x1, y1, color="blue", s=0.5)
+                    # label the graph
+                    plt.title("各芯片数据", size=20)
+                    plt.xlabel("芯片编号", size=15)
+                    plt.ylabel("测量值", size=15)
+
+                    # second graph
+                    plt.subplot(1, 2, 2)
+                    # set values of x and y
+                    x2 = np.linspace(mean - 3 * standard_deviation, mean + 3 * standard_deviation, 100)
+                    y2 = stats.norm.pdf(x2, mean, standard_deviation)
+
+                    # display the value of the mean, variance and standard deviation on graph on appropriate position
                     proper_separation = [mean + standard_deviation, (1 / (3 * standard_deviation)) / 10]
                     plt.text(proper_separation[0], stats.norm.pdf(mean, mean, standard_deviation),
                              f"平均值 = {round(mean, 4)}", size=12)
@@ -312,20 +309,21 @@ def rapid_calculation_interface(file_name, cumulative_color):
                              stats.norm.pdf(mean, mean, standard_deviation) - 2 * proper_separation[1],
                              f"标准差 = {round(standard_deviation, 4)}", size=12)
 
-                    # provide different colors
-                    if cumulative_color % 6 == 1:
-                        color = "r--"
-                    elif cumulative_color % 6 == 2:
-                        color = "b--"
-                    elif cumulative_color % 6 == 3:
-                        color = "g--"
-                    elif cumulative_color % 6 == 4:
-                        color = "y--"
-                    else:
-                        color = "c--"
-                    plt.plot(x, y, color)
+
+                    # draw the line
+                    plt.plot(x2, y2, color)
+                    # label the graph
+                    x_label = values_data["x轴名称"]
+                    plt.title(values_data["名称"], size=20)
+                    plt.xlabel(x_label, size=15)
+                    plt.ylabel("概率密度", size=15)
+
                     cumulative_color += 1
+
+                    # show the graph
                     plt.show()
+
+                    figure_num[0] += 1
 
                 elif event_data == "概率计算":
                     try:
@@ -341,43 +339,3 @@ def rapid_calculation_interface(file_name, cumulative_color):
                     window_data["概率"].update(
                         value=f"{round(possibility, 4)}%"
                     )
-
-
-        # analysis_interface(mean, variance, standard_deviation, cumulative_color, csv)
-
-'''
-def analysis_interface(mean, variance, standard_deviation, cumulative_color, csv):
-    # -----------------------------------------------------------------
-    #                   Analysis interface
-    # -----------------------------------------------------------------
-    # print the following command in the interface
-    possibility = 0.0
-
-    layout_data = [
-        [sg.Text(f"平均值: {round(mean, 4)}")],
-        [sg.Text(f"平方差: {round(variance, 4)}")],
-        [sg.Text(f"标准差: {round(standard_deviation, 4)}")],
-        [sg.Button("图像"), sg.InputText(key="名称", size=(15, 1)), sg.T("x轴名称："),
-         sg.InputText(key="x轴名称", size=(15, 1))],
-        [sg.Button("概率计算"), sg.Text("最低值："), sg.InputText(key="x1", size=(8, 1)),
-         sg.Text("最高值："), sg.In(key="x2", size=(8, 1)), sg.T("概率："), sg.T(f"{possibility}%", key="概率")],
-        [sg.Button("返回")],
-        [sg.Button("退出")]
-    ]
-
-    window_data = sg.Window("检查", layout_data, size=(800, 500), location=(350, 200), element_justification='c')
-
-    while True:
-        event_data, values_data = window_data.read()
-
-        
-
-
-
-        elif event_data == "返回":
-            window_data.close()
-            return
-
-        elif event_data in ("退出", None):
-            window_data.close()
-            sys.exit() '''
